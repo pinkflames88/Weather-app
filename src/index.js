8a37e52d76c12d2af536dbb35868001d6fc8f344
pinkflames88 <xnaomiGx@gmail.com>
2022-12-05 22:44:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const api = {
   key: "8b59f01b318900ea52653048da259269",
-  base: "https://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/"
 };
 
 const searchbox = document.querySelector(".search-box");
@@ -53,7 +53,7 @@ function dateBuilder(d) {
     "September",
     "October",
     "November",
-    "December",
+    "December"
   ];
   let days = [
     "Sunday",
@@ -62,87 +62,7 @@ function dateBuilder(d) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
-  ];
-
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
-
-function formatDay(timestamp) {
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-}
-
-const api = {
-  key: "8b59f01b318900ea52653048da259269",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
-
-const searchbox = document.querySelector(".search-box");
-searchbox.addEventListener("keypress", setQuery);
-
-function setQuery(evt) {
-  if (evt.keyCode == 13) {
-    getResults(searchbox.value);
-  }
-}
-
-function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults);
-}
-
-function displayResults(weather) {
-  let city = document.querySelector(".location .city");
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-  let now = new Date();
-  let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder(now);
-
-  let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-  let weather_el = document.querySelector(".current .weather");
-  weather_el.innerText = weather.weather[0].main;
-
-  let hilow = document.querySelector(".hi-low");
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
-    weather.main.temp_max
-  )}°c`;
-}
-
-function dateBuilder(d) {
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    "Saturday"
   ];
 
   let day = days[d.getDay()];
@@ -152,6 +72,7 @@ function dateBuilder(d) {
 
   return `${day} ${date} ${month} ${year}`;
 }
+
 
 function displayForecast(response) {
   let forecast = response.data.daily;
