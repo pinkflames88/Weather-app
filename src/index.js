c1798b1332da9011bdbf08ff06facefb3cfca046
pinkflames88 <xnaomiGx@gmail.com>
2022-12-19 11:30:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,24 +23,24 @@
   }
 
   function displayResults(weather) {
-    let city = document.querySelector(".location .city");
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-    let now = new Date();
-    let date = document.querySelector(".location .date");
-    date.innerText = dateBuilder(now);
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
 
-    let temp = document.querySelector(".current .temp");
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-    let weather_el = document.querySelector(".current .weather");
-    weather_el.innerText = weather.weather[0].main;
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerHTML = weather.weather[0].main;
 
-    let hilow = document.querySelector(".hi-low");
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
-      weather.main.temp_max
-    )}°c`;
-  }
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )}°c`;
+}
 
   function dateBuilder(d) {
     let months = [
