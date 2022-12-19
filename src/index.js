92af78ccd532a687c4341dba1cba3fddf3d10092
pinkflames88 <xnaomiGx@gmail.com>
2022-12-19 11:31:26 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@
   const searchbox = document.querySelector(".search-box");
   searchbox.addEventListener("keypress", setQuery);
 
-  function setQuery(evt) {
-    if (evt.keyCode === 13) {
-      getResults(searchbox.value);
-    }
+ function setQuery(evt) {
+  if (evt.keyCode === 13) {
+    evt.preventDefault();
+
+    getResults(searchbox.value);
   }
+}
 
   function getResults(query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
